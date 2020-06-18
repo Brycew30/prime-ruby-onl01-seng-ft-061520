6c8a80e658d39b129428003a176661f9aaf005c8
Bryce Whittington <brycew30@gmail.com>
2020-06-18 19:58:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,18 +4,6 @@ def prime?(number)
  elsif number == 2
    true
  else
-   (2..number/2).none?{int| number % int == 0}
+   (2..number/2).none? {|int| number % int == 0}
  end
-end
-
-
-def prime?(n)
-  if n <= 1
-    return false
-  elsif n <= 3
-    return true
-  else (2..n/2).none? do |x|
-    n % x == 0
-  end
-  end
 end
