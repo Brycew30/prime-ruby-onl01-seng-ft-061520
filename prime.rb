3a6af3eea429d8c16a00790690510055c619f897
Bryce Whittington <brycew30@gmail.com>
2020-06-18 19:48:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,6 +2,15 @@ def prime?(number)
  if number < 2 
    false
  else
-   (2..n/2).none?
- end
+   (2..n/2).none?{int| number % int == 0}
+end
+
+def prime? (n)
+    if n <= 1
+        false
+    elsif n == 2
+        true
+    else 
+        (2..n/2).none? { |i| n % i == 0}
+    end
 end
