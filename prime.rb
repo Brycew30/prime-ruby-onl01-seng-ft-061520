dbbcaffeacd0ce4b1bac74f6cc026073372ef543
Bryce Whittington <brycew30@gmail.com>
2020-06-18 19:49:50 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,13 +7,3 @@ def prime?(number)
   else
     true
 end
-
-def prime? (n)
-    if n <= 1
-        false
-    elsif n == 2
-        true
-    else 
-        (2..n/2).none? { |i| n % i == 0}
-    end
-end
