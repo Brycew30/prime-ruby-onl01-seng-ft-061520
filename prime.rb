990411c493deb7a12c3543710a11e50be8e71349
Bryce Whittington <brycew30@gmail.com>
2020-06-18 19:53:30 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,9 @@
 def prime?(number)
  if number < 2 
    false
- elsif
-   (2..number/2).none?{int| number % int == 0}
-    true
  elsif number == 2
    true
+ else
+   (2..number/2).none?{int| number % int == 0}
  end
 end
